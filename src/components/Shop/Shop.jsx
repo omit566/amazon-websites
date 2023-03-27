578309a2856c676b3d199ea69541b898b32c38e6
Masum Ahmed <omit106ahmed&gmail.com>
2023-03-27 15:25:59 +0600

    add new

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
@@ -13,7 +14,12 @@ useEffect( () => {
     return (
         <div className='shop-container'> 
             <div className="products-container">
-                <h2>Products coming here: {products.length}</h2>
+           {
+                  products.map(product => <Product
+                  key={product.id}
+                  product={product}
+                  ></Product>)
+           }
             </div>
             <div className="cart-container">
                 <h4>Order Summary</h4>
